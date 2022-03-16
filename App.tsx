0d88d17195afbfeb13566b02dcf339935c184ab3
Michael Wong <mwckkwork@gmail.com>
2022-03-16 22:07:06 +0800

    fix all bugs

--- App.tsx
+++ App.tsx
@@ -33,104 +33,136 @@ class GameMessage {
   }
 };
 
-const url = 'wss://gcym2i3l2d.execute-api.us-east-2.amazonaws.com/Prod';
+//const url = 'wss://gcym2i3l2d.execute-api.us-east-2.amazonaws.com/Prod';
+const url = 'ws://localhost:18080';
 // Don't put websocket instance inside App, bcz everythin inside will 
 // be constantly refreshed, thus many connection will be created
-var ws: WebSocket;
-var character: string = "";
-var yourColor: string = "";
+var wsock: WebSocket;
 
 export default function App() {
   const [map, setMap] = useState(emptyMap);
   const [currentTurn, setCurrentTurn] = useState("w");
-  const [isConnect, setIsConnect] = useState(false);
   const [gameMode, setGameMode] = useState("LOCAL"); // LOCAL, REMOTE, BOT??
   const [serverMessages, setServerMessages] = useState<string[]>([]);
+  const [ws, setWebSocket] = useState<WebSocket>(wsock);
+  const [character, setCharacter] = useState("");
+  const [yourColor, setColor] = useState("");
 
   useEffect(() => {
-    // TODO: determin winer
-  }, [map]); // re-run the effect only if currentTurn change
-
-  useEffect(() => {
-    if (!isConnect) return
-
+    if (!character) return
+    
     const serverMessagesList: string[] = [];
-    ws.onopen = () => {
-      serverMessagesList.push('Connected')
-      setServerMessages([...serverMessagesList])
 
+    //serverMessagesList.push("set character " + character)
+
+    if (character === 'H') {
+      serverMessagesList.push("You are " + "HOST")
+    } else if (character === 'P') {
+      serverMessagesList.push("You are " + "PLAAYER2")
+      
+      setColor(currentTurn)
+      
+      ws.send(new GameMessage(characterId, "COLR=" + currentTurn).toJson())
+    } else if (character === 'V') {
+      serverMessagesList.push("You are " + "VIEWER")
+    } else {
+      console.log("unknown character")
+    }
+
+      setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
+  }, [character]); // when character changed
+
+  useEffect(() => {
+    if (!yourColor) return
+    
+    const serverMessagesList: string[] = [];
+
+    //serverMessagesList.push("set color " + yourColor)
+
+    if (yourColor === 'b') {
+      serverMessagesList.push("You are " + "black")
+    } else if (yourColor === 'w') {
+      serverMessagesList.push("You are " + "white")
+    } else {
+      console.log("unknown color")
+    }
+
+      setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
+  }, [yourColor]);
+
+  useEffect(() => {
+    Alert.alert("ws changed")
+    if (!ws) return
+
+
+    ws.onopen = () => {
+    const serverMessagesList: string[] = [];
+      serverMessagesList.push('Connected')
       ws.send(new GameMessage(characterId, "HOST?").toJson())
+      setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
     };
     ws.onclose = (e) => {
+    const serverMessagesList: string[] = [];
       serverMessagesList.push('Disconnected')
-      setServerMessages([...serverMessagesList])
+      setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
     };
     ws.onerror = (e) => {
+    const serverMessagesList: string[] = [];
       const msg = e.message
-      console.log(msg);
+      setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
     };
     ws.onmessage = (e) => {
+      
+      const serverMessagesList: string[] = [];
+      serverMessagesList.push(e.data)
+
       const arr = e.data.split(':')
       const source = arr[0]
       const msg = arr[1]
       if (source === characterId) {
+        setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
         return
       }
-      //console.log(e.data);
 
-      if (character === "" && msg.startsWith("HOST=")) {
-        character = msg.substring(msg.length - 1)
+      serverMessagesList.push("character = " + character + "isNull = " + (character === ""))
 
-        if (character === 'H') {
-          serverMessagesList.push("You are " + "HOST")
-        } else if (character === 'P') {
-          serverMessagesList.push("You are " + "PLAAYER2")
-          ws.send(new GameMessage(characterId, "CURR=" + currentTurn).toJson())
-          yourColor = currentTurn
-          serverMessagesList.push("Your color =  " + yourColor)
-        } else if (character === 'V') {
-          serverMessagesList.push("You are " + "VIEWER")
-        } else {
-          console.log("unknown")
-        }
-      } else if (msg.startsWith("CURR=")) {
+      if (character === "" && msg.startsWith("CHAR=")) {
+        setCharacter(msg.substring(msg.length - 1))
+      } else if (msg.startsWith("COLR=")) {
         if (character !== 'H') {
           serverMessagesList.push("Player2 joined")
         }
         const t = msg.substring(msg.length - 1)
         setCurrentTurn(t)
-        yourColor = (t === "b") ? "w" : "b"
-        serverMessagesList.push("Your color =  " + yourColor)
+        setColor((t === "b") ? "w" : "b")
       } else if (msg.startsWith("NEXT=")) {
         const coords = msg.substring(5).split(',')
         const rowIdx = parseInt(coords[0], 10)
         const colIdx = parseInt(coords[1], 10)
-        
+
         console.log(yourColor, rowIdx, colIdx)
         onPress(rowIdx, colIdx)
       }
 
-      setServerMessages([...serverMessagesList])
+      setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
     };
-  }, [isConnect])
+  })
 
   const submitMessage = (msg: string) => {
     ws.send(msg);
   }
 
   const onConnectGameServer = () => {
-    ws = new WebSocket(url)
-    setIsConnect(true)
+    setWebSocket(new WebSocket(url))
   }
 
   const onDisconnectGameServer = () => {
     ws.close()
-    setIsConnect(false)
   }
 
   const onResetGame = () => {
+    console.log("reset")
     setMap((existingMap) => {
-      //console.log(existingMap)
       const newMap = existingMap.map(row => row.map(cell => ""))
       newMap[3][3] = "b"
       newMap[3][4] = "w"
@@ -141,7 +173,7 @@ export default function App() {
     });
 
     setServerMessages([])
-    setCharacter("")
+    ws.send(new GameMessage(characterId, "RESET").toJson())
   }
 
   const updateMap = (gameboard: string[][], rowIdx: number, colIdx: number, currentTurn: string) => {
@@ -308,11 +340,11 @@ export default function App() {
     // newMap = [...map] cannot make a deep copy for an array of array
     ///////////////////////////////////////////////////////////////////
     const newMap = map.map((row) => { return [...row] }) // deep clone
-    console.log(character + ": currentTurn = "+ currentTurn)
+    console.log(character + ": currentTurn = " + currentTurn)
     const updatedCount = updateMap(newMap, rowIndex, columnIndex, currentTurn)
     if (updatedCount === 0) {
-      console.log("update count = "+ updatedCount)
-      console.log(rowIndex +","+ columnIndex)
+      console.log("update count = " + updatedCount)
+      console.log(rowIndex + "," + columnIndex)
       Alert.alert("Invalid Move");
       return;
     }
@@ -335,6 +367,8 @@ export default function App() {
 
       <View style={styles.header}>
         <Text> Current Turn: {currentTurn === "w" ? "White" : "Black"}</Text>
+        <Text> character: {character}</Text>
+        <Text> color: {yourColor}</Text>
 
 
         <View style={{ backgroundColor: '#ffeece', padding: 5, flexGrow: 1 }}>
