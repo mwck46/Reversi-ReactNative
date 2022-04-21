97f951b3567da33fe2a8d4c4f638a0a80c9316d1
Michael Wong <mwckkwork@gmail.com>
2022-04-21 22:57:27 +0800

    revamp messaging function with server

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import { setStatusBarNetworkActivityIndicatorVisible, StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect, useRef } from "react";
-import { StyleSheet, Text, View, ScrollView, Alert, Button, } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import {
+  StyleSheet, Text, View, ScrollView, Alert, Button,
+  Modal, TouchableOpacity, TextInput
+} from 'react-native';
 import Cell from "./components/Cell";
+import { GameMessage } from "./components/GameMessage";
 import uuid from 'react-native-uuid';
 import Constants from "expo-constants";
 
@@ -18,160 +22,91 @@ const emptyMap = [
   ["", "", "", "", "", "", "", ""],
 ];
 
-class GameMessage {
-  constructor(uuid: string, msg: string) {
-    this.#_data.data = uuid.toString() + ":" + msg
-  }
+enum ColorCode {
+  Black = 0,
+  White = 1,
+}
+const ColorTable = new Map([
+  [0, 'black'],
+  [1, "white"],
+])
 
-  // private
-  #_data = { action: "sendmessage", data: "" }
 
-  toJson() {
-    return JSON.stringify(this.#_data)
-  }
-
-  toString() {
-    return this.#_data.data
-  }
-};
-
-//const url = 'wss://gcym2i3l2d.execute-api.us-east-2.amazonaws.com/Prod';
+//https://stackoverflow.com/a/6310592/9265852
 var url = ""
 if (!manifest?.debuggerHost) {
   url = 'ws://localhost:18080';
 } else {
   url = `ws://${manifest.debuggerHost.split(':').shift()}:18080`;
 }
-//if(Device.isDevice){
-//  console.log("isdevice")
-//  url = 'ws://192.168,:18080'; //https://stackoverflow.com/a/6310592/9265852
-//}
+
 // Don't put websocket instance inside App, bcz everythin inside will 
 // be constantly refreshed, thus many connection will be created
-var wsock: WebSocket;
+var ws: WebSocket;
 
 export default function App() {
   const [map, setMap] = useState(emptyMap);
   const [currentTurn, setCurrentTurn] = useState("w");
   const [gameMode, setGameMode] = useState("LOCAL"); // LOCAL, REMOTE, BOT??
+  const [isStarted, setIsStarted] = useState(false);
   const [serverMessages, setServerMessages] = useState<string[]>([]);
-  const [ws, setWebSocket] = useState<WebSocket>(wsock);
   const [character, setCharacter] = useState("");
-  const [yourColor, setColor] = useState("");
+  const [myColor, setMyColor] = useState<ColorCode>((Math.floor(Math.random())));
+  const [gameId, setGameId] = useState('');
 
   useEffect(() => {
-    if (!character) return
-
-    const serverMessagesList: string[] = [];
-
-    //serverMessagesList.push("set character " + character)
-
-    if (character === 'H') {
-      serverMessagesList.push("You are " + "HOST")
-    } else if (character === 'P') {
-      serverMessagesList.push("You are " + "PLAAYER2")
-
-      setColor(currentTurn)
-
-      ws.send(new GameMessage(characterId, "COLR=" + currentTurn).toJson())
-    } else if (character === 'V') {
-      serverMessagesList.push("You are " + "VIEWER")
-    } else {
-      console.log("unknown character")
-    }
-
-    setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
-  }, [character]); // when character changed
-
-  useEffect(() => {
-    if (!yourColor) return
-
-    const serverMessagesList: string[] = [];
-
-    //serverMessagesList.push("set color " + yourColor)
-
-    if (yourColor === 'b') {
-      serverMessagesList.push("You are " + "black")
-    } else if (yourColor === 'w') {
-      serverMessagesList.push("You are " + "white")
-    } else {
-      console.log("unknown color")
-    }
-
-    setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
-  }, [yourColor]);
-
-  useEffect(() => {
-    //Alert.alert("ws changed")
-    if (!ws) return
-
+    ws = new WebSocket(url);
 
     ws.onopen = () => {
       const serverMessagesList: string[] = [];
-      serverMessagesList.push('Connected')
-      ws.send(new GameMessage(characterId, "HOST?").toJson())
+      serverMessagesList.push('Opened connection')
       setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
     };
     ws.onclose = (e) => {
       const serverMessagesList: string[] = [];
-      serverMessagesList.push('Disconnected')
+      serverMessagesList.push('Closed connection')
       setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
     };
     ws.onerror = (e) => {
       const serverMessagesList: string[] = [];
-      const msg = e.message
+      const msg = (e as WebSocketErrorEvent).message;
+      serverMessages.push(`[WebSocket Error] ${msg}`)
       setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
     };
-    ws.onmessage = (e) => {
 
+    ws.onmessage = (e) => {
       const serverMessagesList: string[] = [];
+      const msgObj = GameMessage.parseFromSocket(e.data);
       //serverMessagesList.push(e.data)
 
-      const arr = e.data.split(':')
-      const source = arr[0]
-      const msg = arr[1]
-      if (source === characterId) {
-        setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
-        return
-      }
-
-      //serverMessagesList.push("character = " + character + "isNull = " + (character === ""))
-
-      if (character === "" && msg.startsWith("CHAR=")) {
-        setCharacter(msg.substring(msg.length - 1))
-      } else if (msg.startsWith("COLR=")) {
-        if (character !== 'H') {
-          serverMessagesList.push("Player2 joined")
+      if (msgObj.sender === "SERVER") {
+        if (msgObj.message === "GAMEID") {
+          setGameId(msgObj.remarks);
+          setIsStarted(true);
+        } else if (msgObj.message === "JOINGAME") {
+          setMyColor(Number(msgObj.remarks));
+        } else if (msgObj.message === "ERROR") {
+          serverMessagesList.push(`[Error] ${msgObj.remarks}`)
+        } else if (msgObj.message === "GAMEOVER") {
+          serverMessagesList.push("WON")
         }
-        const t = msg.substring(msg.length - 1)
-        setCurrentTurn(t)
-        setColor((t === "b") ? "w" : "b")
-      } else if (msg.startsWith("NEXT=")) {
-        const coords = msg.substring(5).split(',')
-        const rowIdx = parseInt(coords[0], 10)
-        const colIdx = parseInt(coords[1], 10)
-
-        console.log(yourColor, rowIdx, colIdx)
-        onPress(rowIdx, colIdx)
+      } else if (msgObj.sender === "RIVAL") {
+        if (msgObj.message === "NEXTMOVE") {
+          const coords = msgObj.remarks.split(',');
+          const rowIdx = parseInt(coords[0], 10);
+          const colIdx = parseInt(coords[1], 10);
+          console.log(myColor, rowIdx, colIdx);
+          onPress(rowIdx, colIdx);
+        }
+      } else {
+        console.log("Message from unknow source received");
       }
 
       setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
     };
   })
 
-  const submitMessage = (msg: string) => {
-    ws.send(msg);
-  }
-
-  const onConnectGameServer = () => {
-    setWebSocket(new WebSocket(url))
-  }
-
-  const onDisconnectGameServer = () => {
-    ws.close()
-  }
-
-  const onResetGame = () => {
+  const onResetGame = useCallback(() => {
     console.log("reset")
     setMap((existingMap) => {
       const newMap = existingMap.map(row => row.map(cell => ""))
@@ -185,7 +120,7 @@ export default function App() {
 
     setServerMessages([])
     ws.send(new GameMessage(characterId, "RESET").toJson())
-  }
+  }, [])
 
   const updateMap = (gameboard: string[][], rowIdx: number, colIdx: number, currentTurn: string) => {
     // Add current
@@ -361,11 +296,11 @@ export default function App() {
     }
 
     const serverMessagesList: string[] = [];
-    if (yourColor === currentTurn) {
-      serverMessagesList.push("Me:"+rowIndex+","+columnIndex)
+    if (myColor === currentTurn) {
+      serverMessagesList.push("Me:" + rowIndex + "," + columnIndex)
       ws.send(new GameMessage(characterId, "NEXT=" + rowIndex + "," + columnIndex).toJson())
-    }else{
-      serverMessagesList.push("Opponent:"+rowIndex+","+columnIndex)
+    } else {
+      serverMessagesList.push("Opponent:" + rowIndex + "," + columnIndex)
     }
     setServerMessages(serverMessages => [...serverMessages, ...serverMessagesList])
 
@@ -373,6 +308,62 @@ export default function App() {
     setCurrentTurn(currentTurn === "w" ? "b" : "w");
   };
 
+  const registerNewGame = useCallback(() => {
+    const msg = new GameMessage(characterId, "NEWGAME", myColor.toString()).toString();
+    ws.send(msg);
+  }, [])
+
+  const joinGame = useCallback(() => {
+    const msg = new GameMessage(characterId, "JOINGAME", gameId).toString();
+    ws.send(msg);
+  }, [gameId])
+
+
+  const renderModal = () => {
+    return (
+      <Modal
+        animationType={"slide"}
+        // transparent={true}
+        visible={!isStarted}
+        style={{ flex: 1, margin: 0 }}
+      >
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,.5)' }}>
+          <Text style={{ fontSize: 64, fontWeight: '800' }}>
+            <Text style={{ color: 'black' }}>R</Text>
+            <Text style={{ color: 'white' }}>E</Text>
+            <Text style={{ color: 'black' }}>V</Text>
+            <Text style={{ color: 'white' }}>E</Text>
+            <Text style={{ color: 'black' }}>R</Text>
+            <Text style={{ color: 'white' }}>S</Text>
+            <Text style={{ color: 'black' }}>I</Text>
+          </Text>
+
+          <TouchableOpacity onPress={registerNewGame}>
+            <Text style={{ fontSize: 32, color: 'white', fontWeight: '500' }}>
+              Host Game
+            </Text>
+          </TouchableOpacity>
+
+
+          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'flex-start', }}>
+            <TouchableOpacity onPress={joinGame}>
+              <Text style={{ fontSize: 32, color: 'white', fontWeight: '500', }}>
+                Join
+              </Text>
+            </TouchableOpacity>
+            <TextInput style={{ marginHorizontal: 10, fontSize: 32, borderWidth: 3, height: 'auto', }}
+              value={gameId}
+              placeholder="Game ID..."
+              onChangeText={newText => setGameId(newText)}
+            >
+            </TextInput>
+
+          </View>
+
+        </View>
+      </Modal>
+    )
+  }
 
   return (
     <View style={styles.container}>
@@ -381,7 +372,7 @@ export default function App() {
         <View style={styles.statusRow}>
           <Text style={styles.status}> Current Turn: {currentTurn === "w" ? "White" : "Black"}</Text>
           <Text style={styles.status}> Character: {character}</Text>
-          <Text style={styles.status}> Color: {yourColor}</Text>
+          <Text style={styles.status}> Color: {myColor}</Text>
         </View>
 
         <View style={styles.logger}>
@@ -400,36 +391,22 @@ export default function App() {
       </View>
 
       <View style={styles.gameboard}>
-        {map.map((row, rowIndex) => (
+        {
+          map.map((row, rowIndex) => (
+            <View key={`row-${rowIndex}`} style={{ flex: 1, flexDirection: "row", }}>
+              {row.map((cell, columnIndex) => (
+                <Cell key={`row-${rowIndex}-col-${columnIndex}`}
+                  cell={cell}
+                  onPress={() => onPress(rowIndex, columnIndex)}
+                />
+              ))}
+            </View>
 
-          <View key={`row-${rowIndex}`} style={styles.row}>
-            {row.map((cell, columnIndex) => (
-              <Cell
-                key={`row-${rowIndex}-col-${columnIndex}`}
-                cell={cell}
-                onPress={() => onPress(rowIndex, columnIndex)}
-              />
-            ))}
-          </View>
-
-        ))}
-      </View>
-      <View style={styles.footer}>
-        <View style={styles.controlPanel}>
-          <View style={styles.button}>
-            <Button title='Connect' onPress={() => onConnectGameServer()}> </Button>
-          </View>
-          <View style={styles.button}>
-            <Button title='Disconnect' onPress={() => onDisconnectGameServer()}></Button>
-          </View>
-          <View style={styles.button}>
-            <Button title='Reset' onPress={() => onResetGame()}></Button>
-          </View>
-        </View>
-
+          ))
+        }
       </View>
 
-      <StatusBar style="auto" />
+      {renderModal()}
     </View>
   );
 }
@@ -477,10 +454,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginTop: 5,
     alignItems: 'center',
-  },
-  row: {
-    flex: 1,
-    flexDirection: "row",
   },
   controlPanel: {
     flex: 1,
